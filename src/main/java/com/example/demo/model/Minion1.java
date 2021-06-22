9b0b1b9293f450c9d57b3cbeca0c2f6a7b8d8354
Gregory Bowles <71733838+Sbowles21@users.noreply.github.com>
2021-06-22 09:00:26 -0500

    users

---
+++ src/main/java/com/example/demo/model/Minion1.java
@@ -0,0 +1,79 @@
+package com.example.demo.model;
+
+import java.util.UUID;
+
+public class Minion1 {
+    private UUID id;
+    private String Name;
+    private int Health;
+    private String Weapon;
+    private int Weapon_damage;
+
+    public Minion1() {
+    }
+
+    public Minion1(UUID id,String name, int health, String weapon, int weapon_damage) {
+        Name = name;
+        Health = health;
+        Weapon = weapon;
+        Weapon_damage = weapon_damage;
+    }
+
+    public Minion1(String name, int health, String weapon, int weapon_damage) {
+        Name = name;
+        Health = health;
+        Weapon = weapon;
+        Weapon_damage = weapon_damage;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getHealth() {
+        return Health;
+    }
+
+    public void setHealth(int health) {
+        Health = health;
+    }
+
+    public String getWeapon() {
+        return Weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        Weapon = weapon;
+    }
+
+    public int getWeapon_damage() {
+        return Weapon_damage;
+    }
+
+    public void setWeapon_damage(int weapon_damage) {
+        Weapon_damage = weapon_damage;
+    }
+
+    @Override
+    public String toString() {
+        return "Minion1{" +
+                "id=" + id +
+                ", Name='" + Name + '\'' +
+                ", Health=" + Health +
+                ", Weapon='" + Weapon + '\'' +
+                ", Weapon_damage=" + Weapon_damage +
+                '}';
+    }
+}
